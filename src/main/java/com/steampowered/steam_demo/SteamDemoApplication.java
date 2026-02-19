921eb8fb94777e503b0866f600f9793defaee9ed
gorkem kaptan <ahmetgorkemkaptan@gmail.com>
2026-02-19 09:42:45 +0300

    Initial commit

---
+++ src/main/java/com/steampowered/steam_demo/SteamDemoApplication.java
@@ -0,0 +1,13 @@
+package com.steampowered.steam_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steampowered/steam_demo/SteamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steampowered.steam_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
