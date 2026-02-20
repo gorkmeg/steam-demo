b36f3ada74c30d12c8b7873f29542f9f4136a2d8
gorkem kaptan <ahmetgorkemkaptan@gmail.com>
2026-02-20 16:41:33 +0300

    added balance to dto

--- src/main/java/com/steampowered/steam_demo/dto/response/UserResponse.java
+++ src/main/java/com/steampowered/steam_demo/dto/response/UserResponse.java
@@ -3,6 +3,7 @@ package com.steampowered.steam_demo.dto.response;
 import com.steampowered.steam_demo.entity.UserType;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
@@ -10,4 +11,5 @@ public class UserResponse {
     private UUID id;
     private String username;
     private UserType userType;
+    private BigDecimal balance;
 }
