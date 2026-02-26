b1c732a8345ef0fd7a400c71119eab6d8ddaac9b
gorkem kaptan <ahmetgorkemkaptan@gmail.com>
2026-02-26 15:16:08 +0300

    Update SteamDemoApplication.java

--- src/main/java/com/steampowered/steam_demo/SteamDemoApplication.java
+++ src/main/java/com/steampowered/steam_demo/SteamDemoApplication.java
@@ -2,8 +2,9 @@ package com.steampowered.steam_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class SteamDemoApplication {
 
 	public static void main(String[] args) {
